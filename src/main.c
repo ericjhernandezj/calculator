f96b4e842ed4ea17d3bf6ad24e41d46e3fa4a2ad
Eric Joel Hernandez Javier <81542075+ericjhernandezj@users.noreply.github.com>
2023-02-25 22:02:22 -0500

    Add basic program structure

--- src/main.c
+++ src/main.c
@@ -10,11 +10,51 @@ double divide(double a, double b);
 
 int main(void)
 {
+    int operation;
+    float numberA, numberB;
+
     puts("----- Command Line Calculator -----");
 
     puts("Press ENTER to start");
     printf(": ");
     getchar();
+
+    clearTerminal();
+
+    puts("What function do you want to execute?");
+
+    puts("1. Addition");
+    puts("2. Subtraction");
+    puts("3. Multiplication");
+    puts("4. Division");
+
+    printf(": ");
+    scanf("%i", &operation);
+
+    printf("\nFirst number: ");
+    scanf("%f", &numberA);
+    printf("Second number: ");
+    scanf("%f", &numberB);
+
+    switch (operation)
+    {
+    case 1:
+        printf("Result: %.3f\n", add(numberA, numberB));
+        break;
+    case 2:
+        printf("Result: %.3f\n", subtract(numberA, numberB));
+        break;
+    case 3:
+        printf("Result: %.3f\n", multiply(numberA, numberB));
+        break;
+    case 4:
+        if (numberB == 0) puts("Undefined");
+        else printf("Result: %.3f\n", divide(numberA, numberB));
+        break;
+    default:
+        puts("ERROR");
+        break;
+    }
 
     return 0;
 }
