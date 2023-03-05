cd848d76e61164fb8482ad38b26b1f0b74ce380a
Eric Joel Hernandez Javier <81542075+ericjhernandezj@users.noreply.github.com>
2023-03-05 13:38:33 -0500

    Add main menu

--- src/main.c
+++ src/main.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "basic_functions.h"
+
 void clearTerminal(void);
 
 int main(void)
 {
+    int choice;
+
+    clearTerminal();
+    puts("----- Calculator -----\n");
+
+    puts("Menu\n");
+    puts("1. Basic Operations");
+    puts("2. Advanced  Operations");
+    puts("3. Unit Conversion");
+    puts("4. Calculus");
+    puts("5. Probability and Statistics");
+    puts("6. Geometry");
+    puts("7. Matrices");
+    puts("8. Algebra");
+    puts("9. Base Conversion (decimal, binary, hexadecimal)");
+    puts("10. Finance");
+
+    printf("\n:");
+    scanf("%i", &choice);
+
     return 0;
 }
 
 void clearTerminal(void)
 {
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
