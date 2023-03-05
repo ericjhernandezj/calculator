1b22e58ea4bb9078b692e2f2af28729e1c09d304
Eric Joel Hernandez Javier <81542075+ericjhernandezj@users.noreply.github.com>
2023-03-05 13:17:00 -0500

    Add basic_functions .c and .h files

--- src/main.c
+++ src/main.c
@@ -3,59 +3,8 @@
 
 void clearTerminal(void);
 
-double add(double a, double b);
-double subtract(double a, double b);
-double multiply(double a, double b);
-double divide(double a, double b);
-
 int main(void)
 {
-    int operation;
-    float numberA, numberB;
-
-    puts("----- Command Line Calculator -----");
-
-    puts("Press ENTER to start");
-    printf(": ");
-    getchar();
-
-    clearTerminal();
-
-    puts("What function do you want to execute?");
-
-    puts("1. Addition");
-    puts("2. Subtraction");
-    puts("3. Multiplication");
-    puts("4. Division");
-
-    printf(": ");
-    scanf("%i", &operation);
-
-    printf("\nFirst number: ");
-    scanf("%f", &numberA);
-    printf("Second number: ");
-    scanf("%f", &numberB);
-
-    switch (operation)
-    {
-    case 1:
-        printf("Result: %.3f\n", add(numberA, numberB));
-        break;
-    case 2:
-        printf("Result: %.3f\n", subtract(numberA, numberB));
-        break;
-    case 3:
-        printf("Result: %.3f\n", multiply(numberA, numberB));
-        break;
-    case 4:
-        if (numberB == 0) puts("Undefined");
-        else printf("Result: %.3f\n", divide(numberA, numberB));
-        break;
-    default:
-        puts("ERROR");
-        break;
-    }
-
     return 0;
 }
 
@@ -66,24 +15,4 @@ void clearTerminal(void)
     #else
         system("clear");
     #endif
-}
-
-double add(double a, double b)
-{
-    return a + b;
-}
-
-double subtract(double a, double b)
-{
-    return a - b;
-}
-
-double multiply(double a, double b)
-{
-    return a * b;
-}
-
-double divide(double a, double b)
-{
-    return a / b;
 }
