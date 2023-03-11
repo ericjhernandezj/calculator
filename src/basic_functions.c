4fe135f66fad8a26edce0e955042ee969bd2a50e
Eric Joel Hernandez Javier <81542075+ericjhernandezj@users.noreply.github.com>
2023-03-10 22:27:24 -0500

    Add menu for Basic Operations section

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void clearTerminal(void);
 
 int basic_functions(void)
 {
-    puts("Basic Operations");
+    clearTerminal();
+
+    puts("----- Basic Operations -----");
+
+    puts("What operation do you want to perform?");
+    puts("1. Addition");
+    puts("2. Subtraction");
+    puts("3. Multiplication");
+    puts("4. Division");
 
     return 0;
 }
@@ -25,4 +36,13 @@ double multiply(double a, double b)
 double divide(double a, double b)
 {
     return a / b;
+}
+
+void clearTerminal(void)
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
